ef6c9a0d00ff6a1fb6a543b722c87af6823601be
Kenneth Bryan L. Chung <kenchung@Kenneths-MacBook-Pro.local>
2015-10-21 04:47:46 +0800

    added scenarios to groceries index

--- app/controllers/api/groceries_controller.rb
+++ app/controllers/api/groceries_controller.rb
@@ -1,3 +1,4 @@
+require 'date'
 class Api::GroceriesController < ApiController
 
   before_action :find_obj, except: [:index, :create]
@@ -12,12 +13,13 @@ class Api::GroceriesController < ApiController
   end
 
   def index
+    half = Date.today.day.to_i/15.0 <= 1 ? (1) : (2)
     @grocery = Grocery.where(month: params[:month], year: params[:year], half: params[:half]).first
     if @grocery.present?
       build_index
-    # elsif Grocery.is_latest?({month: params[:month], year: params[:year], half: params[:half]})
-    #   @grocery = Grocery.latest_grocery
-    #   build_index
+    elsif Date.today.month.to_i == params[:month].to_i && Date.today.year.to_i == params[:year].to_i && half == params[:half].to_i
+      @grocery = Grocery.first_or_copy_latest
+      build_index
     else
       render json: {message: 'no data available'}
     end
