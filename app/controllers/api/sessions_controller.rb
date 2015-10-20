14ed9c74fadd7f7979e4a4e4b877fa302f5938b3
rav <rav_bay@yahoo.com>
2015-10-21 00:31:22 +0800

    removed pry on session create

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -6,9 +6,7 @@ class Api::SessionsController < ApiController
   def create
     google_user = Oauth::GoogleAuthenticator.new(params).fetch_user
     user = User.find_or_initialize_by(email: google_user[:emails].first["value"])
-
     user.update(access_token: google_user[:access_token])
-    binding.pry
     render json: {token: user.auth_token, user: user}
   end
 
